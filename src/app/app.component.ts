42e8a855649d6456992d7eeed0283fb517e89485
joseph517 <alvarovergara1993@hotmail.com>
2023-01-28 01:42:10 -0500

    app consulta del clima

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ServiceWeatherService} from './services/service-weather.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  weather: any
   title = 'weather-app';
+  
+  constructor( private serviceWeatherService: ServiceWeatherService){}
+
+  ngOnInit() {
+    
+  }
+
+  getWeather(cityName: string) {
+    this.serviceWeatherService.getWeather(cityName)
+      .subscribe(
+        res => this.weather = res,
+        err => alert('Ciudad incorrecta') 
+    )
+  }
+
+  submitLocation(cityName: HTMLInputElement) {
+    this.getWeather(cityName.value)
+    cityName.value = ''
+    cityName.focus()
+    
+    return false
+    
+  }
+
 }
---
+++ src/app/services/service-weather.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceWeatherService {
+
+
+  apiKey: string = 'd8704d2b4f1ffd6fb2340b7a92ae80ad';
+  URI: string = ''
+
+  constructor(private httpClient: HttpClient) { 
+    this.URI = `https://api.openweathermap.org/data/2.5/weather?&appid=${this.apiKey}&units=metric&q=`
+  }
+
+  getWeather(cityName: string) {
+    return this.httpClient.get(`${this.URI}${cityName}`)
+  }
+}
