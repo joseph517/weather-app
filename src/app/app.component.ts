838daf2b521e01a14c31d55dec9bf436477a3aa4
joseph517 <alvarovergara1993@hotmail.com>
2023-08-14 22:27:05 -0500

    feat: add funtion getIconWeather and add styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,28 +9,33 @@ import {ServiceWeatherService} from './services/service-weather.service'
 export class AppComponent implements OnInit {
   weather: any
   title = 'weather-app';
-  
+  icon : any
+
   constructor( private serviceWeatherService: ServiceWeatherService){}
 
   ngOnInit() {
-    
+
   }
 
   getWeather(cityName: string) {
     this.serviceWeatherService.getWeather(cityName)
       .subscribe(
-        res => this.weather = res,
-        err => alert('Ciudad incorrecta') 
+        res => {
+          this.weather = res;
+          this.icon = this.serviceWeatherService.getIconWeather(this.weather.weather[0].icon);
+        },
+        err => alert('Ciudad incorrecta')
     )
   }
+
 
   submitLocation(cityName: HTMLInputElement) {
     this.getWeather(cityName.value)
     cityName.value = ''
     cityName.focus()
-    
+
     return false
-    
+
   }
 
 }
--- src/app/services/service-weather.service.ts
+++ src/app/services/service-weather.service.ts
@@ -10,11 +10,14 @@ export class ServiceWeatherService {
   apiKey: string = 'd8704d2b4f1ffd6fb2340b7a92ae80ad';
   URI: string = ''
 
-  constructor(private httpClient: HttpClient) { 
+  constructor(private httpClient: HttpClient) {
     this.URI = `https://api.openweathermap.org/data/2.5/weather?&appid=${this.apiKey}&units=metric&q=`
   }
 
   getWeather(cityName: string) {
     return this.httpClient.get(`${this.URI}${cityName}`)
+  }
+  getIconWeather(icon: string){
+    return `https://openweathermap.org/img/wn/${icon}@2x.png`
   }
 }
